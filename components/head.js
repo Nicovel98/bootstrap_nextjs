9a45e803789c6230e949d9ec6c9fb301a7214188
Nicovel98 <nicovel98@gmail.com>
2021-05-20 14:37:07 -0500

    Links Navbar

---
+++ components/head.js
@@ -0,0 +1,5 @@
+<Head>
+        <title>Bootstrap 5 CDN</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />        
+</Head>
--- components/navbar.js
+++ components/navbar.js
@@ -1,4 +1,5 @@
 import { Navbar, Nav} from 'react-bootstrap';
+import Link from "next/link";
 
 export default function Navbar1() {
   return ( 
@@ -9,8 +10,12 @@ export default function Navbar1() {
           <Navbar.Toggle className="mr-auto" aria-controls="responsive-navbar-nav"/>                    
           <Navbar.Collapse id="responsive-navbar-nav">            
             <Nav className="me-auto">
-              <Nav.Link className="pt-3 pb-1" href="/">Inicio</Nav.Link>
-              <Nav.Link className="pt-3 pb-1" href="../consumir/consumir">Consumir</Nav.Link>            
+              <Link href="/" passHref>
+                <Nav.Link className="pt-3 pb-1">Inicio</Nav.Link>
+              </Link>
+              <Link href="/consumir/consumir" passHref>
+                <Nav.Link className="pt-3 pb-1">Consumir</Nav.Link>            
+              </Link>              
             </Nav> 
           </Navbar.Collapse>         
         </div>        
